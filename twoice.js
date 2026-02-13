2be4b7b465788b4e45139cc2a1b7f6c943623f97
jkn-code <injiart@mail.ru>
2026-02-13 20:07:09 +0500

    Очередь
    Добавлена очередь сообщений

--- twoice.js
+++ twoice.js
@@ -3,6 +3,8 @@ const speaker = require('speakertts')
 var fs = require('fs');
 
 let channelName
+let messages = []
+let busy = false
 
 if (fs.existsSync('cfg.txt')) {
     let lines = fs.readFileSync('cfg.txt')?.toString().split("\n");
@@ -16,7 +18,6 @@ if (fs.existsSync('cfg.txt')) {
     if (channelName) {
         speak('Говорение начато')
 
-        var fs = require('fs');
         const tmi = require('tmi.js');
         const client = new tmi.Client({
             connection: {
@@ -32,7 +33,7 @@ if (fs.existsSync('cfg.txt')) {
             // console.log(`${tags['display-name']}: ${message}`);
 
             if (!getIgnore().includes(tags['display-name']))
-                speak(tags['display-name'] + ' говорит ' + message)
+                messages.unshift(tags['display-name'] + ' говорит ' + message)
         });
     }
     else speak('Канал в настройках не указан')
@@ -42,15 +43,31 @@ else speak('Файла cfg.txt нетути')
 
 function speak(txt) {
     console.log(txt);
-    speaker.speak(txt, '', 1, 'UTF-8')
+    busy = true
+    speaker.speak(txt, '', 1, 'UTF-8', () => { busy = false })
 }
 
 function getIgnore() {
     let ot = []
     let lines = fs.readFileSync('cfg.txt')?.toString().split("\n");
-    for (let i in lines) 
+    for (let i in lines)
         if (i > 1) ot.push(lines[i].trim())
     return ot
 }
 
+order_message()
+function order_message() {
+    if (!busy) {
+        let msg = messages.pop()
+        if (msg) speak(msg)
+    }
+
+    setTimeout(() => {
+        order_message()
+    }, 1000)
+}
+
+
+
+// to exe
 // pkg -t node*-win-x64 twoice.js
